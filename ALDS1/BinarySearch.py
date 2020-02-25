1c735bf60ab507db20a5cb38ad97042157811139
skyeanka <zyunkoukan@gmail.com>
2020-02-26 01:08:58 +0900

    add BinarySearch

--- ALDS1/BinarySearch.py
+++ ALDS1/BinarySearch.py
@@ -1,22 +1,29 @@
-def is_contained(num, sorted_list):
-    middle_index = len(sorted_list)//2
-    if num > sorted_list[middle_index]:
-        is_contained(num, sorted_list[middle_index:])
-    elif num < sorted_list[middle_index]:
-        is_contained(num, sorted_list[:middle_index])
-    elif num == sorted_list[middle_index]:
-        return True
-    else:
-        return False
+# not found suitable BinarySearch module
+
+def binary_search(a, x, lo=0, hi=None):
+    """Return True if list a in item x, assuming a is sorted.
+    """
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if a[mid] < x:
+            lo = mid+1
+        elif a[mid] > x:
+            hi = mid
+        else:
+            return True
+    return False
 
 same_num_count = 0
 firstlist_num = int(input())
-firstlist = set(input().split())
-sorted_firstlist = sorted(firstlist)
+sorted_firstlist = sorted(set(input().split()))
 secondlist_num = int(input())
 secondlist = set(input().split())
 
 for n in secondlist:
-    if is_contained(n, sorted_firstlist):
+    if binary_search(sorted_firstlist, n):
         same_num_count += 1
 print(same_num_count)
--- README.md
+++ README.md
@@ -2,8 +2,17 @@ aoj-exercise
 ## About the repo
 [AOJ](http://judge.u-aizu.ac.jp/onlinejudge/index.jsp)というオンラインジャッジプログラミングの履歴
 
-## 言語について
-大体Python
+## 実装について
+Python
+理想は、学習的実装と実用的な実装の二種類の実装をする。
+時間がない場合は、学習的実装は大枠だけで、実用的な実装で対応する。
 
-## 進め方
-時間がないので、本の解説を参考に実装する
+4. データ構造
+
+  |            | feature       | python      |
+  | ---------- | ------------- | ----------- |
+  | STACK      | FILO          | list, deque |
+  | QUEUE      | FIFO          | deque       |
+  | LINKEDLIST | has each next | deque       |
+
+  
